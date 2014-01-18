9e2618f949ec524e22d60f10ba989848521b76ad
FuchsG <guillaumfuchs@gmail.com>
2014-01-18 14:27:07 +0100

    delta

--- testPeps/testPeps/Computations.cpp
+++ testPeps/testPeps/Computations.cpp
@@ -37,20 +37,26 @@ int main(){
 	double coeff[size] = {1.};// , .2, .2, .2, .2};
 	int N = 5;
 	int samples = 50000;
-	double t = .8;
+	double t = .4;
 
-	int H = 8;
+	int H = 10;
 
 	PnlMat *past = pnl_mat_create(size, H+1);
+	PnlVect *delta = pnl_vect_create(size);
+	PnlVect *ic_delta = pnl_vect_create(size);
 
 	Bs bs(size, r, NULL, sigma, spot, NULL);
 	Basket opt(strike, coeff, T, N, size);
 	MonteCarlo mc(&bs, &opt, rng, 0.1, samples);
 
 	bs.simul_market(past, H, t, rng);
-	pnl_mat_print(past);
-	mc.price(past, t, prix, ic);
-	cout << "Prix: " << prix << " Ic: " << ic << endl;
+	mc.delta(past, t, delta, ic_delta);
+	pnl_vect_print(delta);
+	pnl_vect_print(ic_delta);
 	system("pause");
 	return 0;
+}
+
+double delta_theorique(double sigma, double T, double S, double K, double r){
+
 }
